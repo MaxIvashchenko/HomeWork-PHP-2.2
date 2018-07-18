87ebe6908127419e7bddf36d22ecf5fd2410e11d
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-07-18 13:46:41 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,38 +1,38 @@
-<?php
-session_start();
-if (!empty($_GET["name"])) {
-	$tests=[];
-	$tests = file_get_contents('./tests/'.$_GET['name']);
-	$tests = json_decode($tests,true);
-}
-$_SESSION['tests'] = $tests;
-?>
-
 <!DOCTYPE html>
 <html lang="ru">
-<html>
+
 <head>
-	<meta charset="utf-8">
-	<title>Домашнее задание к лекции 2.2</title>
+    <title>Домашнее задание к лекции 2.2</title>
+    <meta charset="UTF-8">
 </head>
-<html>
 <body>
-	<h3>Пройди тест</h3>
-	<form action="send.php" method="POST">
-	<?php  foreach ($tests as $test) {?>
-		
-		<fieldset>
-		<legend><?= $test['question'];?></legend>
-		<?php foreach ($test['answers'] as $key => $answer ){ ?>
-			<label><input type="radio" name="<?= $test['title'];?>" value="<?= $key;?>"><?= $answer['variant']; ?></label>
-		<?php } ?>
-		</fieldset>
-
-		<?php } ?>	
-		<input type="submit" value="Отправить">
-	</form>
-<br><br><br>
-
-	<p><a href="admin.php">Вернуться к добавлению теста</a></p>
+<form enctype="multipart/form-data" action="admin.php" method="post">
+  <h2>Выберите файл</h2>
+  <input type="file" name="testFile"><br><br>
+  <input type="submit" value="Отправить тест">
+</form>
+<?php
+if (!empty($_FILES["testFile"]["name"]))  {
+    $filename=$_FILES["testFile"]["name"];
+    $test_array=explode(".", $filename);
+    
+      if ($test_array[1] == "json")  
+      {
+        move_uploaded_file($_FILES["testFile"]["tmp_name"], "tests/" . $_FILES["testFile"]["name"]);
+        echo "<i>Тест отправлен</i> <br><br>";
+?>
+       <form><input type="submit" formaction="list.php" value="Выбрать тест"><br><br></form>
+<?php     
+      }
+    else
+      {
+        echo "<i>Ошибка</i>";
+      }
+  }
+  else
+      {
+        echo "<i>Тест не загружен</i>";
+      }
+?>
 </body>
 </html>
